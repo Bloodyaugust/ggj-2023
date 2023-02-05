1c3ac3f81014347edd4d9e8ba5002401a6c4b6e3
Bloodyaugust <greysonrichey@gmail.com>
2023-02-05 13:39:33 -0700

    Add art assets for current stuff
    - Add assets for background elements
    - Add parallax scrolling

--- actors/Player.gd
+++ actors/Player.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
+@onready var _better_camera = get_tree().get_first_node_in_group("better_cam")
 @onready var _ship: Ship = $"./Ship"
 
 func _physics_process(_delta: float) -> void:
   var _move_direction: Vector2 = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
 
   _ship.move(_move_direction)
+
+  _better_camera.set_target_position(_ship.global_position)
